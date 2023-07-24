d63862bd83b45878383a964a268ec1a135354053
Tyller Mendoza <tyllermendoza@Tyllers-MacBook-Air.local>
2023-07-24 05:29:07 -0400

    new-5

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
+fetch('https://regres.in/api/users', {
+    method: 'POST',
+    headers: {
+        'Content -Type' : 'applacation/json'
+    },
+    body: JSON. stringify({
+        name: 'User 1'
+    })
+}). then (res => {
+    return res.JSON()
+    })
+    .then (data => console. log (data))
+    .catch(error => console. log ( 'ERROR'))
+let change = true
+
 const menu = document.querySelector('#moble-menu')
 const menuLinks = document.querySelector('.navbar__menu')
 
 menu.addEventListener('click', function() {
-    menu.classList.toggle('is-active')
-    menuLinks.classList.toggle('active')
-})
+    change =!change
+    
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+
+    change ?  menu.classList.toggle('is-active') : menuLinks.classList.toggle('active')
+});
